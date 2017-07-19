0a6b4dfe782bd0878b61ec1a1543e3acabdf40b6
Alexander Diaz <john2005_16@yahoo.com>
2017-07-19 09:15:00 -0500

    Hola Heroku 2

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 3001
+
 app.get('/', function (req, res) {
   res.send('Hola Heroku')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
